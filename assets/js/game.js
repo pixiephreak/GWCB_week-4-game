6648e6c6dcdb971ce6b7481b1e6c0f7f22b93555
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-04 00:52:15 -0500

    complete html. start writing Character class.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-console.log('connected')
+
+Character = function(sprite, name, health, attack, counterAttack){
+	this.health = health;
+	this.attack = attack;
+	this.counterAttack = counterAttack;
+
+	$(this).click(function() {
+	  alert( "Handler for .click() called." );
+	});
+}
