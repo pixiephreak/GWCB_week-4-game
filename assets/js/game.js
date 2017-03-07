c9f43f8af5fcf60ac296f7fee1b5e143859cc6b6
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-07 03:32:57 -0500

    refactor displayMenu and character-sprite click event  to use Player class

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,17 +2,21 @@
 // (if yourchar true , then )
 //consolidate into state obj
 //call render on state change
+var random = function(x,y){
+		return Math.floor(Math.random()*x)+y;
+	}
+
 var game = {
 	title: 'This Game',
-	sprites: ['https://placehold.it/200/200','https://placehold.it/200/200','https://placehold.it/200/200','https://placehold.it/200/200'],
 	introduction: 'Here, I give an overview of the game play and goal',
 	instructions1:'Choose any player to begin',
 	instructions2: 'Choose an opponent',
 	instructions3: 'Click to attack your opponent',
-	//on click, player will be added to game using the class constructor and parameters will be passed down from click function.
-	//Also, the img/data for the sprite should be spliced from array, leaving only enemies
-	//for now I'll jsut use this placeholder,
-	enemy: {sprite:'https://placehold.it/200/200',name:'thisone',health:'0',attack:'0'},
+	characters: [{sprite:'https://placehold.it/200/200', name:'thisone'},
+				 {sprite:'https://placehold.it/200/200', name:'thistwo'},
+				 {sprite:'https://placehold.it/200/200', name:'thisthree'},
+				 {sprite:'https://placehold.it/200/200', name:'thisfour'}
+				 ],
 	//add a result to the game object upon win. for now, use this placeholder.
 	result: 'You Won!'
 }
@@ -26,23 +30,34 @@ Character = function(sprite, name, health, attack){
 
 game.displayMenu = function(){
 	$('#main').prepend(`<h1> ${game.title} </h1>`,`<h3>${game.introduction}</h3>`);
-	$('#main').append(`<h2 class='text-center'>${game.instructions1}<h2>`)
-	game.sprites.forEach(function(sprite){
-		var charImage = HTMLsprite.replace('%data%', sprite);
+	$('#main').append(`<h2 class='text-center'>${game.instructions1}<h2>`);
+
+	for (let i=0; i<this.characters.length; i++){
+		var sprite = this.characters[i].sprite;
+		var charImageAlt = HTMLspriteAlt.replace('%data%', this.characters[i].name)
+		var charImage = charImageAlt.replace('%data%', sprite);
 		$('#characters').append(charImage);
-	})
+	}
+	// game.characters.forEach(function(sprite){
+	// 	var charImage = HTMLsprite.replace('%data%', sprite);
+	// 	$('#characters').append(charImage);
+	// })
+	// for(let i=0; i<game.characterData.length; i++){
+	// 	game.characters.push(new Character(game.characterData[i].sprite,game.characterData[i].name, game.characterData[i].health, game.characterData[i].attack));
+	// 	}
+
 
 }
 
 //TO-DO finish removing hardcoded from html
 game.displayEnemies = function(){
-	$('#main').toggleClass( "hidden" );
-	$('#game-stage').toggleClass("hidden");
+	$('#main').toggleClass( 'hidden' );
+	$('#game-stage').toggleClass('hidden');
 	$('#game-stage').prepend(`<h2 class='text-center'>${game.instructions2}<h2>`);
 	var charImage = HTMLspritefig.replace('%imgdata%', game.player.sprite);
 	var charFig = charImage.replace('%playername%', game.player.name);
 		$('#player').append(charFig);
-	game.sprites.forEach(function(sprite){
+	game.characters.forEach(function(sprite){
 		var enemyImage = HTMLenemysprite.replace('%data%', game.enemy.sprite);
 		$('#enemy-choices').append(enemyImage);
 	})
@@ -52,8 +67,8 @@ game.displayEnemies = function(){
 }
 
 game.displayBattle = function(){
-	$('#game-stage').toggleClass("hidden");
-	$('#fight-stage').toggleClass("hidden");
+	$('#game-stage').toggleClass('hidden');
+	$('#fight-stage').toggleClass('hidden');
 	//TO-DO: find a more consice wat to replace string data
 	$('#fight-stage').prepend(`<h2 class='text-center'>${game.instructions3}<h2>`);
 	var playerImage = HTMLplayerfig.replace('%imgdata%', game.player.sprite);
@@ -68,30 +83,50 @@ game.displayBattle = function(){
 		$('#fight-enemy').append(enemyAttack);
 	var playerResult = `<h3 id="player-result">${game.player.health}</h3>`
 	var enemyResult =  `<h3 id="defender-result">${game.enemy.health}</h3>`
-
-
-
 }
 
 game.displayComplete = function(){
-	$('#main').prepend(`<h1> ${game.title} </h1>`,`<h3>${game.result}</h3>`);
+	$('#fight-stage').toggleClass('hidden');
+	$('#game-complete').toggleClass('hidden')
+	$('#game-complete').prepend(`<h1> ${game.title} </h1>`,`<h3>${game.result}</h3>`);
+}
+
+game.restart = function(){
+ 	$('#game-complete').toggleClass('hidden');
+ 	//TO-DO: use some sort of boolean switch to toggle classes instead of hard-coding
 }
 
 $(document).ready(function(){
 
     $('.character-sprite').on('click',function(event){
-        var imgSrc = $(this).attr("src");
-        var name = $(this).attr("alt")
-        game.player = new Character(imgSrc, name, 0, 0);
-        var index = game.sprites.indexOf(imgSrc);
-        game.sprites.splice(0,1);
-        game.displayEnemies();
+        var sprite = $(this).attr("src");
+        var name = $(this).attr("alt");
+        var health = random(200, 40);
+        var attack = random(40,5);
+        // create Player subclass
+        var Player = function(counterAttack){
+            'use strict';
+            Character.call(this, sprite, name, health, attack );
+        };
+        Player.prototype= Object.create(Character.prototype);
+        Player.prototype.constructor = Player;
+        var counterAttack = attack;
+        game.player = new Player(counterAttack);
+        console.log(game.characters, game.players)
+        function findPlayer(value) {
+            return value.name === name;
+        }
+        var index = game.characters.indexOf(game.characters.find(findPlayer));
+        game.characters.splice(index,1);
+        console.log(game.characters);
+        game.displayEnemies;
+
     });
 
      $('#enemy-choices').on('click', '.enemy-choice', function(event){
         imgSrc = $(this).attr("src");
         name = $(this).attr("alt")
-        game.enemy = new Character(imgSrc, name, 0, 0);
+        game.enemy = new Character(imgSrc, name, HEALTHPOINTS, ATTACK);
         console.log('enemy created')
         game.displayBattle();
     });
@@ -108,12 +143,16 @@ $(document).ready(function(){
     	console.log(game.player.health);
     })
 
+    $('#play-again').on('click', function(event){
+    	console.log('click');
+    	game.restart();
+    })
+
 });
 
 
 
 game.displayMenu();
-
 
 
 
--- assets/js/library.js
+++ assets/js/library.js
@@ -1,7 +1,7 @@
 
 
-
-var HTMLsprite = "<img alt='character' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive character-sprite' src=%data% />";
+var HTMLspriteAlt = "<img alt='%data%' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive character-sprite' src=%data% />";
+var HTMLsprite = "<img alt='%data%' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive character-sprite' src=%data% />";
 var HTMLspritefig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded" alt="player"><figcaption class="figure-caption">Name: %playername%</figcaption></figure>'
 var HTMLenemysprite = "<img alt='character' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive enemy-choice' src=%data% />";
 var HTMLplayerfig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded" alt="player"><figcaption class="figure-caption">Name: %playername% Health:%health% Attack Potency:%attack%</figcaption></figure>'
