296132798a16b2bb08819111a990b0df89c67635
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-07 13:59:27 -0500

    claculate and store health and counter attack on attack in fight click event

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,9 +34,9 @@ game.displayMenu = function(){
 	$('#main').prepend(`<h1> ${game.title} </h1>`,`<h3>${game.introduction}</h3>`);
 	$('#main').append(`<h2 class='text-center'>${game.instructions1}<h2>`);
 	game.characters.forEach(function(character){
-		var newCharacter = new Character(character.sprite, character.name, random(200,40), random(40,5));
+		var newCharacter = new Character(character.sprite, character.name, random(100,100), random(20,5));
 		game.newCharacters.push(newCharacter)
-		player.charImage = HTMLspritefig.replace('%imgdata%', newCharacter.sprite).replace('%altdata%', newCharacter.name).replace('%playername%', newCharacter.name).replace('%playerhealth%', newCharacter.health);
+		player.charImage = HTMLspritefig.replace('%imgdata%', newCharacter.sprite).replace('%altdata%', newCharacter.name).replace('%playername%', newCharacter.name).replace('%playerhealth%', newCharacter.health).replace('%playerattack%', newCharacter.attack);
 		$('#characters').append(player.charImage);
 	})
 	console.log(game.newCharacters);
@@ -48,12 +48,12 @@ game.displayEnemies = function(){
 	$('#game-stage').toggleClass('hidden');
 	$('#game-stage').prepend(`<h2 class='text-center'>${game.instructions2}<h2>`);
 	console.log(game.player);
-	player.charImage = HTMLspritefig.replace('%imgdata%', game.player.sprite).replace('%altdata%', game.player.name).replace('%playername%', game.player.name).replace('%playerhealth%', game.player.health);
+	player.charImage = HTMLspritefig.replace('%imgdata%', game.player.sprite).replace('%altdata%', game.player.name).replace('%playername%', game.player.name).replace('%playerhealth%', game.player.health).replace('%playerattack%', game.player.attack);
 	$('#player').append(player.charImage);
 	game.newCharacters.forEach(function(character){
 		var enemy = new Character(character.sprite, character.name, character.health, character.attack);
 		game.enemies.push(enemy);
-		enemyImage = HTMLenemyspritefig.replace('%imgdata%', character.sprite).replace('%altdata%', character.name).replace('%playername%', character.name).replace('%playerhealth%', character.health);
+		enemyImage = HTMLenemyspritefig.replace('%imgdata%', character.sprite).replace('%altdata%', character.name).replace('%playername%', character.name).replace('%playerhealth%', character.health).replace('%playerattack%', character.attack);
 		$('#enemy-choices').append(enemyImage);
 	})
 
@@ -64,8 +64,8 @@ game.displayEnemies = function(){
 game.displayBattle = function(){
 	$('#game-stage').toggleClass('hidden');
 	$('#fight-stage').toggleClass('hidden');
-	var HTMLenemyspritefig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded enemy-sprite" alt="%altdata%"><figcaption class="figure-caption">Name: %playername% Health: %playerhealth%</figcaption></figure>'
-	game.enemy.charImage = HTMLenemyspritefig.replace('%imgdata%', game.enemy.sprite).replace('%altdata%', game.enemy.name).replace('%playername%', game.enemy.name).replace('%playerhealth%', game.enemy.health);
+	var HTMLenemyspritefig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded enemy-sprite" alt="%altdata%"><figcaption class="figure-caption">Name: %playername% Health: %playerhealth% Attack Potency: %playerattack%</figcaption></figure>'
+	game.enemy.charImage = HTMLenemyspritefig.replace('%imgdata%', game.enemy.sprite).replace('%altdata%', game.enemy.name).replace('%playername%', game.enemy.name).replace('%playerhealth%', game.enemy.health).replace('%playerattack%', game.enemy.attack);
 	$('#fight-stage').prepend(`<h2 class='text-center'>${game.instructions3}<h2>`);
 	$('#fight-player').append(player.charImage);
 	$('#fight-enemy').append(game.enemy.charImage);
@@ -88,29 +88,25 @@ $(document).ready(function(){
 
     $('.character-sprite').on('click', function(event){
     	console.log($(this))
-    	var name = $(this).attr("alt")
-    	console.log(name);
+    	var name = $(this).attr("alt");
         var match;
         for (var i = 0; i < game.newCharacters.length; i++) {
            if (game.newCharacters[i].name == name)
                 match = game.newCharacters[i];
         }
-
         // create Player subclass
-        var Player = function(counterAttack){
+        var Player = function(counter){
             'use strict';
             Character.call(this, match.sprite, match.name, match.health, match.attack );
+            this.counterAttack = counter;
         };
         Player.prototype= Object.create(Character.prototype);
         Player.prototype.constructor = Player;
-        var counterAttack = match.attack;
-        game.player = new Player(counterAttack);
-        console.log(match, game.player);
+        var counter = match.attack;
+        game.player = new Player(counter);
         // splice player from newCharacters
-        console.log(game.newCharacters);
        	var index = game.newCharacters.indexOf(match);
        	game.newCharacters.splice(index,1);
-       	console.log(game.newCharacters);
         game.displayEnemies();
 
     });
@@ -125,7 +121,14 @@ $(document).ready(function(){
     });
 
     $('#fight-button').on('click', function(event){
-    	console.log('click')
+
+    	console.log(`player attack:${game.player.attack}, playercoutner:${game.player.counterAttack}`)
+
+    	game.player.health = game.player.health - game.enemy.attack;
+    	game.enemy.health = game.enemy.health - game.player.attack;
+    	game.player.attack = game.player.attack + game.player.counterAttack;
+    	console.log(`now player:${game.player.health} now enemy${game.enemy.health}`)
+    	// $('figcaption').htlm(`Name: ${game.player.name} Health: ${game.player.health}`)
     	// Whenever the player clicks attack, their character damages the defender.
     	// The opponent will lose HP (health points).
     	// These points are displayed at the bottom of the defender's picture.
--- assets/js/library.js
+++ assets/js/library.js
@@ -1,12 +1,2 @@
-var HTMLspritefig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded character-sprite" alt="%altdata%"><figcaption class="figure-caption">Name: %playername% Health: %playerhealth%</figcaption></figure>'
-var HTMLenemyspritefig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded enemy-sprite" alt="%altdata%"><figcaption class="figure-caption">Name: %playername% Health: %playerhealth%</figcaption></figure>'
-
-
-
-
-var HTMLspriteAlt = "<img alt='%data%' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive character-sprite' src=%data% />";
-var HTMLsprite = "<img alt='%data%' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive character-sprite' src=%data% />";
-
-var HTMLenemyspriteAlt = "<img alt='%data%' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive enemy-choice' src=%data% />";
-var HTMLenemysprite = "<img alt='character' class= 'col-md-3 col-xs-6 img img-thumbnail img-responsive enemy-choice' src=%data% />";
-var HTMLplayerfig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded" alt="player"><figcaption class="figure-caption">Name: %playername% Health:%health% Attack Potency:%attack%</figcaption></figure>'
+var HTMLspritefig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded character-sprite" alt="%altdata%"><figcaption class="figure-caption">Name: %playername% Health: %playerhealth% Attack Potency: %playerattack%</figcaption></figure>'
+var HTMLenemyspritefig = '<figure class="figure"> <img src="%imgdata%" class="figure-img img-fluid rounded enemy-sprite" alt="%altdata%"><figcaption class="figure-caption">Name: %playername% Health: %playerhealth% Attack Potency: %playerattack%</figcaption></figure>'
